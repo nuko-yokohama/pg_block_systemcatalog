0232d3d19051d9aa08160d9f6bb6c19a3ca23d1b
nuko.yokohama <nuko.yokohama@gmail.com>
2017-09-25 10:41:49 +0900

    add regression test.

---
+++ sql/pg_block_systemcatalog.sql
@@ -0,0 +1,55 @@
+CREATE ROLE monitor_role;
+CREATE USER monitor;
+CREATE USER test;
+GRANT monitor_role TO monitor;
+
+CREATE TABLE tmp (id int, data text);
+
+SET pg_block_systemcatalog.allow_role = '';
+
+-- superuser
+SELECT CURRENT_USER;
+
+SELECT relname FROM pg_class WHERE relname = 'tmp';
+SELECT table_name FROM information_schema.tables WHERE table_name = 'tmp';
+SELECT relname FROM pg_stat_user_tables WHERE relname = 'tmp';
+
+-- test(error)
+SET ROLE test;
+SELECT CURRENT_USER;
+
+SELECT relname FROM pg_class WHERE relname = 'tmp';
+SELECT table_name FROM information_schema.tables WHERE table_name = 'tmp';
+SELECT relname FROM pg_stat_user_tables WHERE relname = 'tmp';
+
+RESET ROLE;
+
+-- monitor(error)
+SET ROLE monitor;
+
+SELECT CURRENT_USER;
+
+SELECT relname FROM pg_class WHERE relname = 'tmp';
+SELECT table_name FROM information_schema.tables WHERE table_name = 'tmp';
+SELECT relname FROM pg_stat_user_tables WHERE relname = 'tmp';
+
+RESET ROLE;
+
+SET pg_block_systemcatalog.allow_role = 'monitor';
+
+-- monitor(error)
+SET ROLE monitor;
+
+SELECT CURRENT_USER;
+
+SELECT relname FROM pg_class WHERE relname = 'tmp';
+SELECT table_name FROM information_schema.tables WHERE table_name = 'tmp';
+SELECT relname FROM pg_stat_user_tables WHERE relname = 'tmp';
+
+RESET ROLE;
+
+DROP TABLE tmp;
+REVOKE monitor_role FROM monitor; 
+DROP ROLE test;
+DROP ROLE monitor;
+DROP ROLE monitor_role;
